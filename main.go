069793e2813bb6f7b48ac01db8e0e32eea193102
Filippo Fonseca <filifonsecacagnazzo@gmail.com>
2024-08-05 10:51:25 -0600

    hello handler

--- main.go
+++ main.go
@@ -6,6 +6,21 @@ import (
 	"net/http"
 )
 
+func helloHandler (w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 Not Found", http.StatusNotFound)
+		return
+	}
+
+	if r.Method != "GET" {
+		http.Error(w, "Method is not supported.", http.StatusNotFound)
+		return
+	}
+
+	fmt.Fprintf(w, "Hello!")
+
+}
+
 func main() {
 	fileServer := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fileServer)
@@ -13,7 +28,8 @@ func main() {
 	http.HandleFunc("/hello", helloHandler)
 
 	fmt.Printf("Starting server at port 8080\n")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
 		log.Fatal(err)
 	}
 }
